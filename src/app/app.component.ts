0835a0336ca1e94c43938dad265940e217b05a0b
Pawel Wojtak <pawel.wojtak@globallogic.com>
2019-04-25 10:59:58 +0200

    updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
+import { Router } from '@angular/router';
+import { Variable } from '@angular/compiler/src/render3/r3_ast';
+import { DefaultValueAccessor } from '@angular/forms';
+import { forEach } from '@angular/router/src/utils/collection';
+import { analyzeAndValidateNgModules } from '@angular/compiler';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import json from '../assets/data/settings.json';
+
+export interface Users {
+  value: any;
+  viewValue: String;
+}
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +20,42 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'vm-app';
+  appTitle = 'Virtual Machine Manager';
+  version = 'V01.00.38';
+
+  selectedUserW78000;
+  selectedUserW78001;
+  selectedUserW78002;
+  selectedUserW105030;
+  selectedUserW105031;
+  selectedUserW105032;
+
+
+  userArray = [this.selectedUserW78000, this.selectedUserW78001, this.selectedUserW78002, this.selectedUserW105030, this.selectedUserW105031, this.selectedUserW105032];
+
+  users : Users[] = [
+    {value: 0, viewValue: 'Free'},
+    {value: 1, viewValue: 'Pawel'},
+    {value: 2, viewValue: 'Joanna'},
+    {value: 3, viewValue: 'Galyna'},
+    {value: 4, viewValue: 'Grzegorz'},
+    {value: 5, viewValue: 'Raman'},
+    {value: 6, viewValue: 'Michal'},
+    {value: 7, viewValue: 'Krystian'}
+  ]
+   ngOnInit(): void {
+    let counter = 0;
+    console.dir(json)
+    for (let key in json) {
+      console.log(key)
+      this.userArray[counter] = json[key]
+      console.log(json[key])
+      counter++;
+    }
+   }
+   selected (){
+     this.userArray.forEach(element => {
+       console.log(element)
+     });
+   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CookieService } from 'ngx-cookie';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -10,9 +15,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [CookieService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
